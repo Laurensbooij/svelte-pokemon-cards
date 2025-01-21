02287dd8bf7fb9e7b0c7d66cd72f1e41b2495db9
Laurensbooij <Laurens_booij1@hotmail.com>
2025-01-21 20:29:27 +0100

    feat: add layout with header component

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,16 @@
+<div class="header-container" >
+    <h2>Pokemon Cards</h2>
+</div>
+
+<style>
+    .header-container {
+        padding: var(--spacing-16) var(--spacing-24);
+        background: var(--colors-primary);
+        border-bottom: 1px solid var(--colors-border);
+    }
+
+    h2 {
+        color: var(--colors-text-light);
+        margin: 0;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Header } from '$components'
+
+	let { children } = $props()
+</script>
+
+<svelte:head>
+	<title>Pokemon cards</title>
+</svelte:head>
+
+<div class="container">
+	<Header />
+	{@render children()}
+</div>
+
+<style>
+	.container {
+		display: grid;
+		grid-template-rows: min-content auto;
+		overflow: hidden;
+		height: 100vh;
+	}
+</style>
